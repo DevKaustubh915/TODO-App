d7a60de9c2dbffea5802c20095b4345bef66cf6d
DevKaustubh <kaustubh915b@gmail.com>
2026-01-16 09:45:39 +0530

    All features addes. adding todo, updating todo, deleting todo

--- src/main/java/com/example/TODO/App/Controller/TodoController.java
+++ src/main/java/com/example/TODO/App/Controller/TodoController.java
@@ -1,9 +1,10 @@
 package com.example.TODO.App.Controller;
 
+import com.example.TODO.App.Entity.TodoEntity;
 import com.example.TODO.App.Repository.TodoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TodoController {
@@ -21,4 +22,34 @@ public class TodoController {
         model.addAttribute("todos" , todoRepository.findAll());
         return "index";
     }
+
+    @PostMapping("/add")
+    public String add(@RequestParam String title){
+        TodoEntity newTodo = TodoEntity.builder()
+                .title(title)
+                .completed(false)
+                .build();
+
+        todoRepository.save(newTodo);
+        return "redirect:/";
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable Long id){
+        TodoEntity existingTodo = todoRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Todo not found "+ id));
+        existingTodo.setCompleted(true);
+        todoRepository.save(existingTodo);
+        return "redirect:/";
+
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id){
+        TodoEntity existingTodo = todoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Todo not found "+ id));
+        todoRepository.delete(existingTodo);
+        return "redirect:/";
+    }
+
 }
